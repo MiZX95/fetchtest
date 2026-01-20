e14231aa35ef30a09f3595efe6d5e8330c5a0cef
MichiGitinger <Michael-wedler95@gmx.de>
2026-01-20 13:50:47 +0100

    main clas fetsch ink. logic

---
+++ fetchinger.java
@@ -0,0 +1,16 @@
+public class Main{
+
+
+    /*
+    * Main class for main logic
+    * @author WM
+    *
+    * ersellt 20.01.2026
+    *
+    * */
+    public static void main(String[] args){
+
+
+        System.out.print("hallo, welt");
+    }
+}
